963ada6273412fddf5249e765360ae7192e7ceee
Deiran Margarido <46607842+deiranmargarido@users.noreply.github.com>
2024-01-22 21:14:09 -0400

    portfolio
    novo

--- assets/formapagamento-814f4f95.js
+++
@@ -1,1 +0,0 @@
-import{av as o,aw as e}from"./index-66469cf1.js";const n=o("formapagamento",{state:()=>({formapagamento:[]}),getters:{getFormaPagamento(a){return a.formapagamento}},actions:{async list(a){await e.get("/formapagamento",{params:a}).then(t=>{this.formapagamento=t.data.formaspagamento})}}});export{n as u};
